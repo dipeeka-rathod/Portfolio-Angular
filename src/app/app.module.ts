92304adde55d7959e138643fb8c580e1d2250aa4
dipeeka-rathod <46849168+dipeeka-rathod@users.noreply.github.com>
2023-02-15 23:53:28 +0530

    basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './component/home-page/home-page.component';
+import { SearchBarComponent } from './component/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
